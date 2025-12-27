5c07696bc6e05450b6d9a552e358a7e002eb0a3c
Piotr Bacior <piotr.bacior@microsoft.wsei.edu.pl>
2025-12-27 03:05:38 +0100

    com7 - program.cs - komentarze do dotychczasowego kodu

--- ExpenseManager/Program.cs
+++ ExpenseManager/Program.cs
@@ -15,22 +15,27 @@ namespace ExpenseManager
             Console.WriteLine("Menadżer wydatków - Start Programu\n");
             Console.WriteLine("Autor - Piotr Bacior\n");
 
+            // Tworzę instancję kontekstu bazy danych i sprawdzam połączenie
             using (var db = new ExpenseContext())
             {
+                // jeżeli połączenie się powiedzie, wyświetlam komunikat o połączeniu
                 if (db.Database.CanConnect())
                 {
                     Console.WriteLine("Sukces! Połączono z bazą danych.");
 
+                    // definiuję zmienną count, która przechowuje liczbę wydatków w bazie danych
                     int count = db.Expenses.Count();
                     Console.WriteLine($"Aktualnie w bazie jest {count} wydatków.");
                 }
 
+                // jeżeli połączenie się nie powiedzie, wyświetlam komunikat o błędzie 
                 else
                 {
                     Console.WriteLine("Błąd! Nie udało się połączyć z bazą.");
                 }
             }
 
+            // Czekam na naciśnięcie klawisza przez użytkownika przed zakończeniem programu
             Console.WriteLine("Naciśnij dowolny klawisz...");
             Console.ReadKey();
         }
