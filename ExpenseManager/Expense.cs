4a8b89620c01a4c3e26b083a03ee6d34dea4d729
Piotr Bacior <piotr.bacior@microsoft.wsei.edu.pl>
2025-12-27 02:43:31 +0100

    com3 - Expense.cs - komenatrze do właściwości klasy

--- ExpenseManager/Expense.cs
+++ ExpenseManager/Expense.cs
@@ -5,9 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ExpenseManager
-{ 
+{
+    // Zaczynam od klasy Expense - reprezentuje pojedynczy wydatek w systemie zarządzania wydatkami 
     public class Expense
     {
+        // poniżej znajdują się właściwości klasy Expense 
         public int Id { get; set; }
         public DateTime Date { get; set; }
         public string Description { get; set; }
---
+++ ExpenseManager/ExpenseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseManager
+{
+    public class ExpenseContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=expenses.db");
+        }
+    }
+}
