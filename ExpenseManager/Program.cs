cd0c0e7b23bf72e3209c04e91c667aac17810574
Piotr Bacior <piotr.bacior@microsoft.wsei.edu.pl>
2025-12-27 07:58:33 +0100

    com9 - program.cs - refakturyzacja pod właściwą wersję zapisywania plików w bazie

--- ExpenseManager/Program.cs
+++ ExpenseManager/Program.cs
@@ -12,61 +12,35 @@ namespace ExpenseManager
         static void Main(string[] args)
         {
             // Witam się z użytkownikiem 
-            Console.WriteLine("Menadżer wydatków - Start Programu\n");
+            Console.WriteLine("Menadżer wydatków v1.0- Start Programu\n");
             Console.WriteLine("Autor - Piotr Bacior\n");
 
-            // Dokonuję refaktoryzacji kodu, aby wymuszał utworzenie bazy danych i wyświetlanie odpowiednich komunikatów o błedach
-
-            // Próbuję połączyć się z bazą danych i wyświetlić odpowiednie komunikaty
-            try
+            // Tworzę kontekst bazy danych
+            using (var db = new ExpenseContext())
             {
-                // Tworzę kontekst bazy danych i sprawdzam połączenie
-                using (var db = new ExpenseContext())
+                // Upewniam się, że baza danych jest utworzona
+                db.Database.EnsureCreated();
+
+                // Tworzę zmienną sterującą pętlą menu o nazwie "exit"
+                bool exit = false;
+
+                // Główna pętla programu 
+                while (!exit)
                 {
-                    // Upewniam się, że baza danych jest utworzona - jeśli nie istnieje, zostanie utworzona
-                    db.Database.EnsureCreated();
+                    Console.WriteLine("=-=-=-=-= MENU PROGRAMU =-=-=-=-= ");
+                    Console.WriteLine("1. Dodaj nowy wydatek.");
+                    Console.WriteLine("2. Wyświetl wszystkie wydatki");
+                    Console.WriteLine("3. Wyjdź z programu");
+                    Console.Write("Wybierz opcję: ");
 
-                    // Sprawdzam, czy mogę połączyć się z bazą danych
-                    if (db.Database.CanConnect())
-                    {
-                        Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
-                        Console.WriteLine("Sukces! Połączono z bazą danych.\n");
-                        Console.WriteLine($"Lokalizacja bazy: {System.IO.Path.GetFullPath("expences.db")}\n");
+                    string input = Console.ReadLine();
 
-                        // Liczenie i wyświetlanie liczby wydatków w bazie danych
-                        int count = db.Expenses.Count();
-                        Console.WriteLine($"Aktualnie w bazie jest {count} wydatków.\n");
-                        Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
-                    }
-
-                    // Jeśli nie udało się połączyć, wyświetlam odpowiedni komunikat
-                    else
-                    {
-                        Console.WriteLine("Błąd. CanConnect zwróciło false, ale nie rzuciło żadnego wyjątku.");
-                    }
+                    
                 }
             }
 
-            // Łapię wszelkie wyjątki i wyświetlam komunikaty o błędach
-            catch (Exception ex)
-            {
-                // Wyświetlam komunikat o błędzie w kolorze czerwonym 
-                Console.ForegroundColor = ConsoleColor.Red;                 
-                Console.WriteLine("UWAGA! Wystąpił krytyczyny błąd: ");
-                Console.WriteLine(ex.Message);
-
-                // Jeśli istnieje wyjątek wewnętrzny, wyświetlam jego szczegóły
-                if (ex.InnerException != null)
-                {
-                    Console.WriteLine("Szczegóły błędu wewnętrznego: ");
-                    Console.WriteLine(ex.InnerException.Message);
-                }
-
-                Console.ResetColor();       // Resetuję kolor konsoli do domyślnego
-            }
-
-            // Czekam na naciśnięcie klawisza przed zamknięciem programu
-            Console.WriteLine("\nNaciśnij dowolny klawisz, aby zamknąć...");
+                // Czekam na naciśnięcie klawisza przed zamknięciem programu
+                Console.WriteLine("\nNaciśnij dowolny klawisz, aby zamknąć...");
             Console.ReadKey();
         }
     }
