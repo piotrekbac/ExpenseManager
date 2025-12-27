1835f615676fb3089e555dece16691950bc7d17d
Piotr Bacior <piotr.bacior@microsoft.wsei.edu.pl>
2025-12-27 02:45:59 +0100

    com5 ExpenesContext.cs - komenatrze do kodu konfiguracji bazy danych

--- ExpenseManager/ExpenseContext.cs
+++ ExpenseManager/ExpenseContext.cs
@@ -5,14 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
+// Piotr Bacior
+
 namespace ExpenseManager
 {
+    // ExpenseContext - kontekst bazy danych dla zarządzania wydatkami
     public class ExpenseContext : DbContext
     {
+        // DbSet reprezentujący kolekcję wydatków w bazie danych 
         public DbSet<Expense> Expenses { get; set; }
 
+        // Konfiguracja kontekstu bazy danych
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            // Użycie bazy danych SQLite, będzie zapisywana w pliku o nazwie expenses.db 
             optionsBuilder.UseSqlite("Data Source=expenses.db");
         }
     }
